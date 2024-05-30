98103d72972366414e76d442e1f04db919527403
PechimuthuSivakumar <82590999+PechimuthuSivakumar@users.noreply.github.com>
2024-05-30 23:31:11 +0530

    Delete frontend directory

--- frontend/src/Component/tasks/ViewPlans.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ViewPlans = () => {
-  return (
-    <div>ViewPlans</div>
-  )
-}
-
-export default ViewPlans
--- frontend/src/Provider/PlansProvider/index.jsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-const PlansContext = createContext();
-export default PlansContext;
-
