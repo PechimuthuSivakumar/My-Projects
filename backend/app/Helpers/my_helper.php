9c7e0c972b585ba438cd686934b78b2a81a41729
PechimuthuSivakumar <82590999+PechimuthuSivakumar@users.noreply.github.com>
2024-05-30 23:30:45 +0530

    Delete backend directory

--- backend/app/Helpers/my_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function topic_master($id){
-        $db = \config\Database::connect();
-        $table = $db->table('topic_master');
-        $query = $table->select('count(*)')->where('sub_id',$id)->get();
-        return $query->getResult();
-    }
-?>
